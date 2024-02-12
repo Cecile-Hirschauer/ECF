c10546022c42e43c128214f320d4b2f10a658381
cecile hirschauer <cecile.h.75@gmail.com>
2024-02-12 16:06:59 +0100

    Finish implementing and testing crowdfunding contract

--- backend/contracts/Crowdfunding.sol
+++ backend/contracts/Crowdfunding.sol
@@ -25,16 +25,21 @@ contract Crowdfunding is Ownable {
 
     constructor() Ownable(msg.sender){}
 
-    function addCampaign(
+    // ************* CAMPAIGN MANAGEMENT ************* //
+
+    function validateCampaignParameters(
         uint256 _goal,
         string memory _name,
         string memory _description,
         string memory _imageUrl
-    ) external {
+    ) internal pure {
         require(_goal > 0, "Crowdfunding: goal must be greater than 0");
         require(bytes(_name).length > 0, "Crowdfunding: name cannot be empty");
         require(bytes(_description).length > 0, "Crowdfunding: description cannot be empty");
         require(bytes(_imageUrl).length > 0, "Crowdfunding: imageUrl cannot be empty");
+    }
+    function addCampaign(uint256 _goal, string calldata _name, string calldata _description, string calldata _imageUrl) external {
+        validateCampaignParameters(_goal, _name, _description, _imageUrl);
 
         campaigns[nextCampaignId] = Campaign({
             creator: msg.sender,
@@ -50,4 +55,43 @@ contract Crowdfunding is Ownable {
         emit CampaignAdded(nextCampaignId, msg.sender, _name, _goal);
         nextCampaignId++;
     }
+
+    function updateCampaign(uint256 _campaignId, uint256 _goal, string calldata _name, string calldata _description, string calldata _imageUrl) external {
+        require(campaigns[_campaignId].isActive, "Campaign does not exist");
+        require(msg.sender == campaigns[_campaignId].creator || msg.sender == owner(), "Caller is not the creator or the owner");
+
+        validateCampaignParameters(_goal, _name, _description, _imageUrl);
+
+        campaigns[_campaignId].goal = _goal;
+        campaigns[_campaignId].name = _name;
+        campaigns[_campaignId].description = _description;
+        campaigns[_campaignId].imageUrl = _imageUrl;
+
+        emit CampaignUpdated(_campaignId, msg.sender, _name, _goal);
+    }
+
+    function getCampaign(uint256 _campaignId) external view returns (
+        address creator,
+        uint256 goal,
+        uint256 raisedAmount,
+        bool isSuccessful,
+        bool isActive,
+        string memory name,
+        string memory description,
+        string memory imageUrl
+    ) {
+        require(campaigns[_campaignId].isActive, "Campaign does not exist");
+        Campaign storage campaign = campaigns[_campaignId];
+        return (
+            campaign.creator,
+            campaign.goal,
+            campaign.raisedAmount,
+            campaign.isSuccessful,
+            campaign.isActive,
+            campaign.name,
+            campaign.description,
+            campaign.imageUrl
+        );
+    }
+
 }
--- backend/test/unit/Crowdfunding.test.js
+++ backend/test/unit/Crowdfunding.test.js
@@ -16,7 +16,9 @@ describe('Crowdfunding Tests', function () {
         assert.equal(admin, owner.address)
     })
 
-    describe('Campaign creation tests', function () {
+    describe('Campaign management tests', function () {
+
+        // Add Campaign
         it('Should revert if the goal is less than 1', async function () {
             await expect(crowdfundingContract.addCampaign(
                 0, // _goal
@@ -83,6 +85,80 @@ describe('Crowdfunding Tests', function () {
                 );
         });
 
+        describe('Get and update Campaign', function () {
+            beforeEach(async function () {
+                await crowdfundingContract.connect(addr1).addCampaign(
+                    100, // _goal
+                    "Test Campaign", // _name
+                    "Test Description", // _description
+                    "Test ImageUrl" // _imageUrl
+                )
+            })
+
+            it('Should revert if the campaign does not exist', async function () {
+                await expect(crowdfundingContract.getCampaign(1))
+                    .to.be.revertedWith("Campaign does not exist")
+            })
+
+            it('Should get the campaign information', async function () {
+                let campaign = await crowdfundingContract.getCampaign(0)
+                assert.equal(campaign.goal, 100)
+                assert.equal(campaign.name, "Test Campaign")
+                assert.equal(campaign.description, "Test Description")
+                assert.equal(campaign.imageUrl, "Test ImageUrl")
+            })
+
+            it('Should revert if the campaign is not updated by his creator or the owner', async function () {
+                await expect(crowdfundingContract.connect(addr2).updateCampaign(
+                    0,
+                    200, // _goal
+                    "Updated Test Campaign", // _name
+                    "Updated Test Description", // _description
+                    "Updated Test ImageUrl" // _imageUrl
+                )).to.be.revertedWith("Caller is not the creator or the owner");
+            })
+
+            it('Should update the campaign information', async function () {
+                await crowdfundingContract.connect(addr1).addCampaign(
+                    100, // _goal
+                    "Test Campaign", // _name
+                    "Test Description", // _description
+                    "Test ImageUrl" // _imageUrl
+                )
+                await crowdfundingContract.connect(addr1).updateCampaign(
+                    0,
+                    200, // _goal
+                    "Updated Test Campaign", // _name
+                    "Updated Test Description", // _description
+                    "Updated Test ImageUrl" // _imageUrl
+                )
+                let campaign = await crowdfundingContract.campaigns(0)
+                assert.equal(campaign.goal, 200)
+                assert.equal(campaign.name, "Updated Test Campaign")
+                assert.equal(campaign.description, "Updated Test Description")
+                assert.equal(campaign.imageUrl, "Updated Test ImageUrl")
+            })
+
+            it('Should emit a CampaignUpdated event with the correct information', async function () {
+                await expect(crowdfundingContract.connect(addr1).updateCampaign(
+                    0,
+                    200, // _goal
+                    "Updated Test Campaign", // _name
+                    "Updated Test Description", // _description
+                    "Updated Test ImageUrl" // _imageUrl
+                ))
+                    .to.emit(crowdfundingContract, 'CampaignUpdated')
+                    .withArgs(
+                        0,
+                        addr1.address,
+                        "Updated Test Campaign",
+                        200
+                    );
+            })
+
+
+
+        })
 
 
     })
